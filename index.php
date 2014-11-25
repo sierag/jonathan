09b025cd38fd4347b43663baa85efc4a827510b4
jonathan.sierag.nl <jonathan@melle.cyso.net>
2014-11-25 22:51:18 +0100

    New version

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="container">
+	        <div class="row">
+	        	<div class="col-md-8 col-md-offset-2">
+		        	<div class="movie embed-responsive embed-responsive-16by9">
+		 		       	<iframe class="embed-responsive-item" src="//www.youtube.com/embed/DBxMfxV1-Hc" frameborder="0" allowfullscreen></iframe>
+		        	</div>
+		        </div>
+	        </div>
+	    </div>
+        <!-- Add your site or application content here -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+    </body>
+</html>
